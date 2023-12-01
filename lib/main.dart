00d3b54158193d165616e59315ac5b56434ee844
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-12-01 10:56:32 -0300

    up files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:market_flex/pages/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 5));
+  FlutterNativeSplash.remove();
   runApp(const MaterialApp(home: PageHome()));
 }
